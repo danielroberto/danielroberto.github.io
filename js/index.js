c0f553a9ac6abe2b2c0dfcbc976322d24345b0f2
Daniel Roberto <droberto@ucsd.edu>
2017-02-25 22:50:47 -0800

    lit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+   $(document).ready(function(){
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 70;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+         
+			 }
+			 else {
+				 $('nav').addClass('fixed');
+         
+			 }
+		});
+	});
+
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
