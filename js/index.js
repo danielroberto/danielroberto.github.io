6ed6f9beec7a889d5bb2e400bb2515bfa757ba37
Daniel Roberto <droberto@ucsd.edu>
2017-02-27 02:19:20 -0800

    fixed js animate scroller

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-
    $(document).ready(function(){
 	   $(window).bind('scroll', function() {
 	   var navHeight = $( window ).height() - 70;
@@ -13,5 +12,20 @@
 			 }
 		});
 	});
+
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
 
 
