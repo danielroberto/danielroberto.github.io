54d54a37f9a0c3270aa391aa4dde4c614d194bb8
Daniel Roberto <droberto@ucsd.edu>
2017-03-04 01:45:41 -0800

    added back to top button and pages

--- js/index.js
+++ js/index.js
@@ -29,3 +29,16 @@ $('a[href*="#"]:not([href="#"])').click(function() {
 
 
 
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(500);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(500);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
